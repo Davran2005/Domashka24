81ae21b2d559a647660baae3b6c3aa915515d124
Davran2005 <davranjoldoshbaev2@gmail.com>
2022-12-06 16:34:32 +0600

    Commit

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+public class Main {
+
+   public static void main(String[] args) {
+//        1. Напишите программу на Java, чтобы создать новый список массивов,
+//        добавить несколько городов, вывести их в консоль.
+//            ArrayList<String> strings=new ArrayList<>();
+//            strings.add("Bishkek");
+//            strings.add("Osh");
+//            strings.add("Naryn");
+
+//        System.out.println(strings);
+//        2. Напишите программу Java для вставки элемента в список массивов в первую позицию.
+//            strings.add(2,"Issyk-Kol");
+//        System.out.println(strings);
+
+//        3. Напишите программу Java для извлечения элемента (по указанному индексу) из заданного списка массивов.
+//            ArrayList<String> num=new ArrayList<>();
+//            num.add("Davran");
+//            num.add("Khafiz");
+//            num.add("Nurik");
+//            num.add("Adi");
+//        System.out.println("num.get() = " + num.get(2));
+//        4. Напишите программу Java для обновления определенного элемента массива заданным элементом.
+//            ArrayList<String>num2=new ArrayList<>();
+//            num2.add("Altynbek");
+//            num2.add("Alibek");
+//            num2.add("Nurseit");
+//            num2.add("Ainazik");
+//        System.out.println("num2.set(\"Abu-Ali\") = " + num2.set(2,"Abu-Ali"));
+//        System.out.println(num2);
+//        5. Напишите программу на Java для удаления третьего элемента из списка массивов.
+//             ArrayList<String>arrayLis=new ArrayList<>();
+//             arrayLis.add("Argen");
+//             arrayLis.add("Nuris");
+//             arrayLis.add("Alisher");
+//             arrayLis.add("Nursultan");
+//             arrayLis.remove(1);
+//             System.out.println(arrayLis);
+//        6. Напишите программу Java для поиска элемента в списке массивов.
+//            ArrayList<String> arrayList = new ArrayList<>();
+//            arrayList.add("argen");
+//            arrayList.add("alisher");
+//            arrayList.add("syimyk");
+//            arrayList.add("davran");
+//
+//        System.out.println("This find by id!");
+//            String index = scanner.nextLine();
+//
+//        for (String s : arrayList) {
+//            if (s.equals(index)) {
+//                System.out.println(s);
+//            }
+//        }
+
+////        7. Напишите программу на Java для сортировки заданного списка массивов.
+//        ArrayList<String>list=new ArrayList<>();
+//        list.add("Temirlan");
+//        list.add("Joodar");
+//        list.add("Azamat");
+//        list.add("Aisulu");
+//        list.add("Arnas");
+//
+//        Collections.sort(list);
+//        System.out.println(list);
+//        }
+
+//        8. Напишите программу на Java для копирования одного списка массивов в другой.
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("Chui");
+//        arrayList.add("Naryn");
+//        arrayList.add("Bishkek");
+//        arrayList.add("Jalal-Abat");
+//        ArrayList<String> addAll = new ArrayList<>();
+//        addAll.addAll(arrayList);
+//        System.out.println("Oridginal" + arrayList);
+//        System.out.println("Copy" + addAll);
+//        9. Напишите программу на Java для перемешивания элементов в списке массивов.
+//        System.out.println("Org"+arrayList);
+//        Collections.shuffle(arrayList);
+//        System.out.println("Naborot"+arrayList);
+//        10. Напишите программу на Java, чтобы поменять местами элементы в списке массивов.
+//        System.out.println("Org"+arrayList);
+//        Collections.reverse(arrayList);
+//        System.out.println("Peremeshenyi"+arrayList);
+//        11. Напишите программу Java для извлечения части массива
+//        List<Integer> list2 = new ArrayList(arrayList);
+//        System.out.println("List" + list2);
+//        List<Integer> list3=list2.subList(1,3);
+//        System.out.println("Sublist"+list3);
+
+
+
+//
+    }
+}
